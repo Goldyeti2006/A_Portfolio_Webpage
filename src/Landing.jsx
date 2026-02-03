2a0aef6f0f7a0e79f7411c1e792c5213c25b8224
Goldyeti2006 <sharanprakashrkasbag@gmail.com>
2026-02-03 22:36:22 +0530

    modified:   src/Landing.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,7 +1,7 @@
 export default function Landing() {
     return (
       <div className="text-left p-6">
-        <h1 className="text-3xl font-bold">Greetings!</h1>
+        <h1 className="text-3xl font-bold">img</h1>
       </div>
     );
   }
